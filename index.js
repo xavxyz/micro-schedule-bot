59d6992f029cd27bc2dd570020cc646b6ecb0130
xavcz <xavier.cazalot@gmail.com>
2017-02-27 09:57:51 +0000

    handle multi message and check for the request from slack slash command

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
 const { send } = require('micro');
+const parse = require('urlencoded-body-parser');
 const rp = require('request-promise');
 const fs = require('fs');
 
-const { WEBHOOK_URL } = process.env;
+const { WEBHOOK_URL, SLASH_TOKEN, ALLOWED_USER } = process.env;
 
 const handleErrors = fn => async (req, res) => {
   try {
     return await fn(req, res);
-  } catch (err) {
-    console.log(err.stack)
-    send(res, 500, 'Something went bad...');
+  } catch ({ stack = '...', statusCode = 500, message = 'No idea what 😵' }) {
+    console.log(stack);
+    send(res, statusCode, `Something went bad: ${message}`);
   }
 }
 
+// the current message to send, start at 0
+let messageToSendIndex = 0;
+
 module.exports = handleErrors(async (req, res) => {
-  const config = fs.readFileSync('_config.json');
-  const { username, icon_url, text } = JSON.parse(config);
   
-  const response = await rp({
-    uri: WEBHOOK_URL,
-    method: 'POST',
-    body: {
-      text,
-      username,
-      icon_url,
-    },
-    json: true,
+  const slashCommand = await parse(req);
+  
+  if (!slashCommand 
+      || slashCommand.token !== SLASH_TOKEN 
+      || slashCommand.user_name !== ALLOWED_USER
+    ) {
+    return {
+      response_type: 'ephemeral',
+      text: `Sorry, that didn\'t work.`,
+    };
+  }
+  
+  const config = fs.readFileSync('_config.json');
+  const { username, icon_url, messages } = JSON.parse(config);
+  
+  const messageObject = messages[messageToSendIndex];
+  
+  send(res, 200, Object.assign({}, messageObject, {
+    username,
+    icon_url,
+    response_type: 'ephemeral',
   });
   
-  return response;
+  messageToSendIndex++;
+  
+  // to move the job scheduler
+  // const slackMessage = await rp({
+  //   uri: WEBHOOK_URL,
+  //   method: 'POST',
+  //   body: Object.assign({}, messageObject, {
+  //     username,
+  //     icon_url,
+  //   }),
+  //   json: true,
+  // });
 });
